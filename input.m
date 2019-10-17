46d9497c15c26e4c3febf7c472d9f50bda12cdd3
tomo <aegis43ina177ricefieldtburi@gmail.com>
2019-10-17 13:33:34 +0900

    add some calcration for longitunal

--- input.m
+++ input.m
@@ -1,9 +1,11 @@
+abc = 2;
+
 function Vt = TailVolume (MArm, Stail, Smain, Mainlong)
-    Vt = MArm * Stail / Smain / Mainlong;
+  Vt = MArm * Stail / Smain / Mainlong;
 endfunction
 
 function Asp = aspect (b, S);
-    Asp = b * b / S;
+  Asp = b * b / S;
 endfunction
 
 function dEps_dAlpha = calcDEpsRate(aw, asp)
