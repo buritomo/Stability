dc25081403852a3b3415354c1d8f5f9a2496085b
tomo <aegis43ina177ricefieldtburi@gmail.com>
2019-10-17 19:06:16 +0900

    finish ver 1

--- Lateral.m
+++ Lateral.m
@@ -1,28 +1,56 @@
-## Copyright (C) 2019 tomohiro
-## 
-## This program is free software: you can redistribute it and/or modify it
-## under the terms of the GNU General Public License as published by
-## the Free Software Foundation, either version 3 of the License, or
-## (at your option) any later version.
-## 
-## This program is distributed in the hope that it will be useful, but
-## WITHOUT ANY WARRANTY; without even the implied warranty of
-## MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-## GNU General Public License for more details.
-## 
-## You should have received a copy of the GNU General Public License
-## along with this program.  If not, see
-## <https://www.gnu.org/licenses/>.
+% not to realize this file as function files
+abc = 3;
 
-## -*- texinfo -*- 
-## @deftypefn {} {@var{retval} =} Lateral (@var{input1}, @var{input2})
-##
-## @seealso{}
-## @end deftypefn
+function Cyb = calcCyb(Sf, S, afin)
+  Cyb = -1 * Sf / S * afin;
+endfunction
 
-## Author: tomohiro <tomohiro@DESKTOP-H0AH0HQ>
-## Created: 2019-10-15
+function Clb = calcClb(ramda, a0, gamma, Cl, yeta)
+  Clbino = -0.006;
+  Clb = a0 * gamma + Cl * tan(yeta);
+  Clb = Clb * (-1 / 6) * (1 + 2 * ramda) / (1 + ramda);
+  Clb = Clb + Clbino;
+endfunction
 
-function retval = Lateral (input1, input2)
+function Cnb = calcCnb(Vfin, afin, Vfus, cbar, b)
+  Cnb = Vfin * afin - 2 * Vfus * cbar / b;  
+endfunction
 
+function Cyp = calcCyp()
+  Cyp = 0;  
+endfunction
+
+function Clp = calcClp(a0, ramda)
+  Clp = -1 * a0 / 12 * (1 + 3 * ramda) / (1 + ramda);  
+endfunction
+
+function Cnp = calcCnp(ramda, Cla, e, asp, CL)
+  Cnp = -1 / 12 * (1 + 3 * ramda) / (1 + ramda);
+  Cnp = Cnp * (1 - Cla / (pi * e * asp))* CL;
+endfunction
+
+function Cyr = calcCyr(Sf, S, afin, lf, b)
+  Cyr = Sf/ S * afin * 2 * lf / b;
+endfunction
+
+function Clr = calcClr(ramda, Cl, zfr, b, lf, S, afin)
+  Clr = 1 / 6 * (1 + 3 * ramda) / (1 + ramda) * Cl;
+  Clr = Clr + zfr / b * lf / S * afin * 2 * lf / b;
+endfunction
+
+function Cnr = calcCnr(ramda, CD, Vfin, afin, lf, b)
+  Cnr = -1 * 6 * (1 + 3 * ramda) / (1 + ramda) * CD;
+  Cnr = Cnr - Vfin * afin * 2 * lf / b;
+endfunction
+
+function Cydr = calcCydr(Sf, S, afin)
+  Cydr = Sf / S * afin;
+endfunction
+
+function Cldr = calcCldr(zfr, b, Sf, S, afin)
+  Cldr = zfr / b * Sf / S * afin;
+endfunction
+
+function Cndr = calcCndr(Vfin, afin)
+  Cndr = -1 * Vfin * afin;
 endfunction
