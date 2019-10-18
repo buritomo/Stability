3f642a5729a862ca9c848de9cc8ec6b0f88dfa43
tomo <aegis43ina177ricefieldtburi@gmail.com>
2019-10-18 14:57:23 +0900

    Update main.m

--- main.m
+++ main.m
@@ -1,5 +1,5 @@
 % define functions
-input;
+setupfs;
 Lateral;
 Longitudinal;
 
@@ -25,12 +25,12 @@ yeta = -0.171 * pi / 180;
 Cog_Z = -0.78;
 
 Le = 4.3;
-Se = 2.406;
+Se = 2.535;
 Ve = TailVolume(Le, Se, S, cbar);
 ae = 5.844;
 
 Lv = 5.25;
-Sv = 2.239;
+Sv = 2.11875;
 Vv = TailVolume(Lv, Sv, S, span);
 av = 5.844;
 zfr = 0.875 - Cog_Z;
@@ -42,39 +42,39 @@ hnw = 0.36;  %Cog:36%M.A.C.
 
 row = 1.165;
 
-ans = zeros(32, 1);
+results = zeros(32, 1);
 
 % calcurate Lengitudinal part
 
 CLa = calcCLalpha (aw, S, ae, Se, dEps);
 
-ans(1, 1) = calcCxu(row, speed, S, T0, CD);
-ans(2, 1) = calcCzu();
-ans(3, 1) = calcCmu();
-ans(4, 1) = calcCxa(CL, CLa, e, Asp);
-ans(5, 1) = calcCza(CLa);
-ans(6, 1) = CLa;
-ans(7, 1) = calcCmalpha(CLa, h, hnw, ae, dEps, Ve, Vfus); 
-ans(8, 1) = calcCmadot(Ve, Le, cbar, ae, dEps);
-ans(9, 1) = calcCzq(Ve, ae);
-ans(10,1) = calcCmq(Ve, Le, cbar, ae);
-ans(11,1) = calcCzde(Se, S, ae);
-ans(12,1) = calcCmde(Se, S, ae);
-ans(13,1) = 0;%make a space
+results(1, 1) = calcCxu(row, speed, S, T0, CD);
+results(2, 1) = calcCzu();
+results(3, 1) = calcCmu();
+results(4, 1) = calcCxa(CL, CLa, e, Asp);
+results(5, 1) = calcCza(CLa);
+results(6, 1) = CLa;
+results(7, 1) = calcCmalpha(CLa, h, hnw, ae, dEps, Ve, Vfus); 
+results(8, 1) = calcCmadot(Ve, Le, cbar, ae, dEps);
+results(9, 1) = calcCzq(Ve, ae);
+results(10,1) = calcCmq(Ve, Le, cbar, ae);
+results(11,1) = calcCzde(Se, S, ae);
+results(12,1) = calcCmde(Se, ae);
+results(13,1) = 0;%make a space
 
 % calcurate Lateral part
-ans(14,1) = calcCyb(Sv, S, av);
-ans(15,1) = calcClb(ramda, a0, gamma, CL, yeta);
-ans(16,1) = calcCnb(Vv, av, Vfus, cbar, span);
-ans(17,1) = calcCyp();
-ans(18,1) = calcClp(a0, ramda);
-ans(19,1) = calcCnp(ramda, CLa, e, Asp, CL);
-ans(20,1) = calcCyr(Sv, S, av, Lv, span);
-ans(21,1) = calcClr(ramda, CL, zfr, span, Lv, S, av);
-ans(22,1) = calcCnr(ramda, CD, Vv, av, Lv, span);
-ans(23,1) = calcCydr(Sv, S, av);
-ans(24,1) = calcCldr(zfr, span, Sv, S, av);
-ans(25,1) = calcCndr(Vv, av);
+results(14,1) = calcCyb(Sv, S, av);
+results(15,1) = calcClb(ramda, a0, gamma, CL, yeta);
+results(16,1) = calcCnb(Vv, av, Vfus, cbar, span);
+results(17,1) = calcCyp();
+results(18,1) = calcClp(a0, ramda);
+results(19,1) = calcCnp(ramda, CLa, e, Asp, CL);
+results(20,1) = calcCyr(Sv, S, av, Lv, span);
+results(21,1) = calcClr(ramda, CL, zfr, span, Lv, S, av);
+results(22,1) = calcCnr(ramda, CD, Vv, av, Lv, span);
+results(23,1) = calcCydr(Sv, S, av);
+results(24,1) = calcCldr(zfr, span, Sv, S, av);
+results(25,1) = calcCndr(Vv, av);
 
 % export results
-csvwrite('results.csv', ans);
+csvwrite('results.csv', results);
